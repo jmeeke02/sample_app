a965e8d1ce11023355f9622338511c327e0bf556
Jordan <jordan.meeker@villanova.edu>
2015-06-14 17:53:29 +0000

    Add a Contact page (solves exercise 3.2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,21 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :password, presence: true, length: { minimum: 6}
     
-    #Returns the hash digest of the given string
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
-                                                      BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-    end
+    class << self
     
-    def User.new_token
-        SecureRandom.urlsafe_base64
+     #Returns the hash digest of the given string
+        def digest(string)
+         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
+                                                      BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+    
+        #returns a random token
+        def new_token
+            SecureRandom.urlsafe_base64
+        end
     end
+
     
     def remember
         self.remember_token = User.new_token
